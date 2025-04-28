0f5f63e84ef0ae6cc3a77c7a09407d0cbb6ea646
nicosikmoraes <nicolas.moraes.ns@gmail.com>
2025-04-28 19:30:35 -0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RegisterComponent,
+            MainScreenComponent,
+        ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/main-screen/main-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-screen',
+  imports: [],
+  templateUrl: './main-screen.component.html',
+  styleUrl: './main-screen.component.css'
+})
+export class MainScreenComponent {
+
+}
